d45a4d5071547eddd78fa0241d372ad9d4effbac
Rémy Ferré <dev@remyferre.net>
2015-07-21 17:36:43 +0100

    Remove blank lines after writing service.yml template

--- packs/service-yaml-writer-base.go
+++ packs/service-yaml-writer-base.go
@@ -2,7 +2,11 @@ package packs
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
 	"path/filepath"
+	"regexp"
+	"strings"
 
 	"github.com/cloud66/starter/common"
 )
@@ -17,5 +21,49 @@ func (w *ServiceYAMLWriterBase) Write(context interface{}) error {
 	if !common.FileExists(filepath.Join(w.TemplateDir, templateName)) {
 		templateName = "service.yml.template" // fall back on generic template
 	}
-	return w.WriteTemplate(templateName, "service.yml", context)
+	err := w.WriteTemplate(templateName, "service.yml", context)
+	if err != nil {
+		return err
+	}
+
+	return w.removeBlankLines()
+}
+
+// NOTE
+// Templates can have a lot of blank lines when some parts of it are evaluated
+// empty. There is currently no way to avoid this with Go templates (See
+// https://github.com/golang/go/issues/9969 for more information)
+// What we do is:
+// 		* Removing all blank lines from the evaluated template
+// 		* If a line contains '##NEWLINE##', replace it by a newline
+func (w *TemplateWriterBase) removeBlankLines() error {
+	fullPath := filepath.Join(w.OutputDir, "service.yml")
+	content, err := ioutil.ReadFile(fullPath)
+	if err != nil {
+		return err
+	}
+
+	blankLinePattern := regexp.MustCompile("^[[:blank:]]*$")
+	newlinePattern := regexp.MustCompile("^##NEWLINE##$")
+
+	lines := strings.Split(string(content), "\n")
+	var newContent []string
+	for _, line := range lines {
+		if !blankLinePattern.MatchString(line) {
+			if newlinePattern.MatchString(line) {
+				newContent = append(newContent, "")
+			} else {
+				newContent = append(newContent, line)
+			}
+		}
+	}
+	newContent = append(newContent, "") // final newline
+
+	file, err := os.Create(fullPath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	_, err = file.WriteString(strings.Join(newContent, "\n"))
+	return err
 }
