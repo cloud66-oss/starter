569d649acc934fd8b17661cadef9684b5a8cc797
Daniel van Gils <daniel@cloud66.com>
2016-03-07 13:25:01 +0100

    add cross compile stuff

---
+++ compile.sh
@@ -0,0 +1,7 @@
+if [ $# -eq 0 ]
+  then
+    echo "No version supplied"
+    exit 1
+fi
+
+gox -ldflags="-X main.VERSION=$1" -osarch="darwin/386 darwin/amd64 linux/386 linux/amd64 windows/386 windows/amd64" -output="compiled/{{.Dir}}_{{.OS}}_{{.Arch}}"
