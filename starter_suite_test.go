96e5b013f673817ffd7f48491fe2609fd9e5f1fb
Daniel van Gils <daniel@cloud66.com>
2016-03-15 14:51:28 +0100

    update test and change stuff for codeship

--- starter_suite_test.go
+++ starter_suite_test.go
@@ -4,6 +4,9 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"testing"
+	"bytes"
+	"time"
+	"os/exec"
 	"github.com/cloud66/starter/common"
 )
 
@@ -15,9 +18,19 @@ func TestStarter(t *testing.T) {
 }
 
 var _ = BeforeSuite(func() {
+	command := exec.Command("git", "describe", "--abbrev=0", "--tags")
+	command_out, _ := command.Output()
+	version := bytes.TrimRight(command_out, "\n")
+	current_date := time.Now().Format("2006-01-02")
+
+
+	err := exec.Command("go", "build", "-ldflags","-X \"main.VERSION=" + string(version) + "\" -X \"main.BUILD_DATE=" + current_date + "\"").Run()
+	Expect(err).NotTo(HaveOccurred())
 	Expect(common.FileExists(binPath)).To(BeTrue())
 })
 
 var _ = AfterSuite(func() {
+	err := exec.Command("rm", binPath).Run()
+	Expect(err).NotTo(HaveOccurred())
 })
 
