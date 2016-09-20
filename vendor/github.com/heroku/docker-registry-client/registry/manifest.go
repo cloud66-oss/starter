999729f8254163264c619d5f723180b0aacbe564
Daniel van Gils <daniel@cloud66.com>
2016-09-20 19:23:52 +0200

    vendorize it like it's 1995.

---
+++ vendor/github.com/heroku/docker-registry-client/registry/manifest.go
@@ -0,0 +1,58 @@
+package registry
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+
+	manifest "github.com/docker/distribution/manifest/schema1"
+)
+
+func (registry *Registry) Manifest(repository, reference string) (*manifest.SignedManifest, error) {
+	url := registry.url("/v2/%s/manifests/%s", repository, reference)
+	registry.Logf("registry.manifest.get url=%s repository=%s reference=%s", url, repository, reference)
+
+	resp, err := registry.Client.Get(url)
+	if resp != nil {
+		defer resp.Body.Close()
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	signedManifest := &manifest.SignedManifest{}
+	err = signedManifest.UnmarshalJSON(body)
+	if err != nil {
+		return nil, err
+	}
+
+	return signedManifest, nil
+}
+
+func (registry *Registry) PutManifest(repository, reference string, signedManifest *manifest.SignedManifest) error {
+	url := registry.url("/v2/%s/manifests/%s", repository, reference)
+	registry.Logf("registry.manifest.put url=%s repository=%s reference=%s", url, repository, reference)
+
+	body, err := signedManifest.MarshalJSON()
+	if err != nil {
+		return err
+	}
+
+	buffer := bytes.NewBuffer(body)
+	req, err := http.NewRequest("PUT", url, buffer)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Content-Type", manifest.MediaTypeManifest)
+	resp, err := registry.Client.Do(req)
+	if resp != nil {
+		defer resp.Body.Close()
+	}
+	return err
+}
