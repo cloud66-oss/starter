77129ebabc05629b2ae11822997812e8fbe6ca4d
Daniel van Gils <daniel@cloud66.com>
2016-10-04 15:10:53 +0200

    support koa

--- common/utils-node.go
+++ common/utils-node.go
@@ -192,7 +192,7 @@ func GetDefaultNodeVersion() string {
 }
 
 func GetSupportedNodeFrameworks() []string {
-	return []string {"express", "loopback", "restify", "actionhero", "hapi", "socket.io"}
+	return []string {"express", "loopback", "restify", "actionhero", "hapi", "socket.io", "koa"}
 }
 
 var defaultNodeVersion = "4.5.0"
