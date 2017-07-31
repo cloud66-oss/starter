1b3b7013f6628c2fffe4d3ea7aa4199234e2adc1
dragospopa <dragos.popa21@yahoo.com>
2017-07-31 12:37:40 +0100

    disabled detection of convertors

--- detect.go
+++ detect.go
@@ -6,21 +6,19 @@ import (
 	"github.com/cloud66/starter/packs/node"
 	"github.com/cloud66/starter/packs/php"
 	"github.com/cloud66/starter/packs/ruby"
-	"github.com/cloud66/starter/packs/compose-to-service-yml"
 	"fmt"
 	"strings"
 	"bufio"
 	"os"
-	"github.com/cloud66/starter/packs/service-yml-to-kubes"
 )
 
 func Detect(rootDir string) ([]packs.Pack, error) {
 	ruby := ruby.Pack{}
 	node := node.Pack{}
 	php := php.Pack{}
-	dockercompose := compose_to_service_yml.Pack{}
-	serviceyml := service_yml_to_kubes.Pack{}
-	detectors := []packs.Detector{dockercompose.Detector(), ruby.Detector(), node.Detector(), php.Detector(), serviceyml.Detector()}
+	//dockercompose := compose_to_service_yml.Pack{}
+	//serviceyml := service_yml_to_kubes.Pack{}
+	detectors := []packs.Detector{ /*dockercompose.Detector(), */ ruby.Detector(), node.Detector(), php.Detector() /*, serviceyml.Detector()*/ }
 	var packs []packs.Pack
 
 	for _, d := range detectors {
@@ -72,8 +70,8 @@ func choosePack(detectedPacks []packs.Pack, noPrompt bool) (packs.Pack, error) {
 				}
 			}
 
-			for i:=0;i<len(detectedPacks);i++{
-				if detectedPacks[i].Name() == "service.yml"{
+			for i := 0; i < len(detectedPacks); i++ {
+				if detectedPacks[i].Name() == "service.yml" {
 					return detectedPacks[i], nil
 				}
 			}
