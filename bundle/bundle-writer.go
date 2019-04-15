8d6ff2646d3a79bc4de4540674f4b5b66563b880
Luca Bartolotti <luca@cloud66.com>
2019-04-15 13:40:48 +0100

    Added the sequence field to the policy struct

--- bundle/bundle-writer.go
+++ bundle/bundle-writer.go
@@ -68,6 +68,7 @@ type BundlePolicy struct {
 	UID      string   `json:"uid"`
 	Name     string   `json:"name"`
 	Selector string   `json:"selector"`
+	Sequence int      `json:"sequence"`
 	Tags     []string `json:"tags"`
 }
 
