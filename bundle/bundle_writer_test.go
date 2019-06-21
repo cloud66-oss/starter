b0cca9feb20d35373ae71fcc51fb51016713fe72
Dima <dima@cloud66.com>
2019-06-21 16:39:51 +0100

    Add more complex test case for dependency resolution

--- bundle/bundle_writer_test.go
+++ bundle/bundle_writer_test.go
@@ -13,6 +13,21 @@ func TestTemplateJSONDependencyTraversal(t *testing.T) {
 	testTemplateJSONDependencyTraversal(t, &templateJSON, []string{"stencils/three"}, []string{"stencils/one", "stencils/two", "stencils/three"})
 	testTemplateJSONDependencyTraversal(t, &templateJSON, []string{"stencils/one", "stencils/two", "stencils/three"}, []string{"stencils/one", "stencils/two", "stencils/three"})
 	testTemplateJSONDependencyTraversal(t, &templateJSON, []string{}, []string{})
+
+	anotherTemplateJSON := generateAnotherTemplateJSON()
+	testTemplateJSONDependencyTraversal(t, &anotherTemplateJSON, []string{}, []string{})
+	testTemplateJSONDependencyTraversal(t, &anotherTemplateJSON, []string{"stencils/one"}, []string{"stencils/one", "stencils/two", "stencils/three", "stencils/four", "stencils/five", "stencils/six"})
+	testTemplateJSONDependencyTraversal(t, &anotherTemplateJSON, []string{"stencils/two"}, []string{"stencils/one", "stencils/two", "stencils/three", "stencils/four", "stencils/five", "stencils/six"})
+	testTemplateJSONDependencyTraversal(t, &anotherTemplateJSON, []string{"stencils/three"}, []string{"stencils/one", "stencils/two", "stencils/three", "stencils/four", "stencils/five", "stencils/six"})
+	testTemplateJSONDependencyTraversal(t, &anotherTemplateJSON, []string{"stencils/four"}, []string{"stencils/four", "stencils/five", "stencils/six"})
+	testTemplateJSONDependencyTraversal(t, &anotherTemplateJSON, []string{"stencils/five"}, []string{"stencils/five"})
+	testTemplateJSONDependencyTraversal(t, &anotherTemplateJSON, []string{"stencils/six"}, []string{"stencils/six"})
+	testTemplateJSONDependencyTraversal(t, &anotherTemplateJSON, []string{"stencils/seven"}, []string{"stencils/seven", "stencils/eight", "stencils/nine"})
+	testTemplateJSONDependencyTraversal(t, &anotherTemplateJSON, []string{"stencils/eight"}, []string{"stencils/eight", "stencils/nine"})
+	testTemplateJSONDependencyTraversal(t, &anotherTemplateJSON, []string{"stencils/nine"}, []string{"stencils/nine"})
+	testTemplateJSONDependencyTraversal(t, &anotherTemplateJSON, []string{"stencils/ten"}, []string{"stencils/ten"})
+	testTemplateJSONDependencyTraversal(t, &anotherTemplateJSON, []string{"stencils/eleven"}, []string{"stencils/eleven"})
+	testTemplateJSONDependencyTraversal(t, &anotherTemplateJSON, []string{"stencils/four", "stencils/seven", "stencils/eleven"}, []string{"stencils/four", "stencils/five", "stencils/six", "stencils/seven", "stencils/eight", "stencils/nine", "stencils/eleven"})
 }
 
 func testTemplateJSONDependencyTraversal(t *testing.T, templateJSON *TemplateJSON, initialComponentNames []string, expectedComponentNames []string) {
@@ -47,6 +62,71 @@ func generateTemplateJSON() TemplateJSON {
 
 	templateStruct := TemplatesStruct{
 		Stencils: []*StencilTemplate{&stencilOne, &stencilTwo, &stencilThree},
+	}
+
+	return TemplateJSON{
+		Templates: &templateStruct,
+	}
+}
+
+func generateAnotherTemplateJSON() TemplateJSON {
+	stencilOne := StencilTemplate{
+		Name:         "one",
+		Dependencies: []string{"stencils/two", "stencils/three"},
+	}
+
+	stencilTwo := StencilTemplate{
+		Name:         "two",
+		Dependencies: []string{"stencils/three"},
+	}
+
+	stencilThree := StencilTemplate{
+		Name:         "three",
+		Dependencies: []string{"stencils/one", "stencils/three", "stencils/four"},
+	}
+
+	stencilFour := StencilTemplate{
+		Name:         "four",
+		Dependencies: []string{"stencils/five", "stencils/six"},
+	}
+
+	stencilFive := StencilTemplate{
+		Name:         "five",
+		Dependencies: []string{},
+	}
+
+	stencilSix := StencilTemplate{
+		Name:         "six",
+		Dependencies: []string{},
+	}
+
+	stencilSeven := StencilTemplate{
+		Name:         "seven",
+		Dependencies: []string{"stencils/eight", "stencils/nine"},
+	}
+
+	stencilEight := StencilTemplate{
+		Name:         "eight",
+		Dependencies: []string{"stencils/nine"},
+	}
+
+	stencilNine := StencilTemplate{
+		Name:         "nine",
+		Dependencies: []string{},
+	}
+
+	stencilTen := StencilTemplate{
+		Name:         "ten",
+		Dependencies: []string{},
+	}
+
+	stencilEleven := StencilTemplate{
+		Name:         "eleven",
+		Dependencies: []string{},
+	}
+
+	templateStruct := TemplatesStruct{
+		Stencils: []*StencilTemplate{&stencilOne, &stencilTwo, &stencilThree, &stencilFour, &stencilFive, &stencilSix, &stencilSeven, &stencilEight, &stencilNine, &stencilTen, &stencilEleven},
 	}
 
 	return TemplateJSON{
