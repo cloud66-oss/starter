77793064a609b91085f365bac25a678f80405041
Luca Bartolotti <luca@cloud66.com>
2019-11-14 16:24:27 +0000

    Pull newest cloud66 version

--- vendor/github.com/cloud66-oss/cloud66/formation.go
+++ vendor/github.com/cloud66-oss/cloud66/formation.go
@@ -15,6 +15,7 @@ type Formation struct {
 	BaseTemplates   []BaseTemplate   `json:"base_templates"`
 	Policies        []Policy         `json:"policies"`
 	Transformations []Transformation `json:"transformations"`
+	Workflows       []Workflow       `json:"workflows"`
 	HelmReleases    []HelmRelease    `json:"helm_releases"`
 	CreatedAt       time.Time        `json:"created_at_iso"`
 	UpdatedAt       time.Time        `json:"updated_at_iso"`
